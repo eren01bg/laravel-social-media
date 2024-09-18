2ba7abc981c6a1689b99f80eded175012469c4d7
Eren Osman <eren01gs@outlook.com>
2024-09-19 00:53:04 +0300

    Implement Username generation, and add ability to update it through the Edit Profile page.

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,16 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'regex:/[\w\-\.]/i'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
         ];
+    }
+
+    public function messages(): array {
+
+        return [
+            'regex' => 'The :attribute may only contain letters, numbers, and dashes'
+        ];
+
     }
 }
